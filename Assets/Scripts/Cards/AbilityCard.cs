4f56804d60f582b3aadcc4c164f1185962403688
William <gwilliaustin@gmail.com>
2020-10-29 19:44:29 -0500

    Added Targeting

---
+++ Assets/Scripts/Cards/AbilityCard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityCard : Card
+{
+    public Sprite Img { get; private set; } = null;
+    public CardPlayEffect PlayEffect { get; private set;} = null;
+
+    public AbilityCard(AbilityCardData data)
+    {
+        Name = data.Name;
+        Img = data.Graphic;
+        PlayEffect = data.PlayEffect;
+
+    }
+
+    public override void Play()
+    {
+        Debug.Log("Player ability card " + Name);
+        ITargetable target = TargetController.CurrnetTarget;
+        PlayEffect.Activate(target);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Camera playerCamera = null;
+
+    InputController Input;
+    TargetController Targeter;
+
+    #region Init
+    private void Awake()
+    {
+        Input = gameObject.AddComponent<InputController>();
+        Targeter = gameObject.AddComponent<TargetController>();
+    }
+
+    private void OnEnable()
+    {
+        Input.Undo += OnUndo;
+        Input.LeftMouse += OnLeftMouse;
+    }
+
+    private void OnDisable()
+    {
+        Input.Undo -= OnUndo;
+        Input.LeftMouse -= OnLeftMouse;
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    #endregion
+    void Update()
+    {
+        
+    }
+
+
+
+    GameObject GetMouseTarget()
+    {
+        Ray mouseRay = playerCamera.ScreenPointToRay(UnityEngine.Input.mousePosition);
+        RaycastHit rayHit;
+        if(Physics.Raycast(mouseRay, out rayHit, Mathf.Infinity))
+        {
+            return rayHit.transform.gameObject;
+        }
+
+        return null;
+    }
+
+
+    #region Input Events
+
+    void OnLeftMouse(eInputStates mouseState)
+    {
+        if(mouseState == eInputStates.Down)
+        {
+            ITargetable possibleTarget = GetMouseTarget().GetComponent<ITargetable>();
+            if(possibleTarget != null)
+            {
+                Targeter.AquireTarget(possibleTarget);
+
+            }
+        }
+    }
+
+    void OnUndo(eInputStates eventState)
+    {
+        Debug.Log("Got undo event");
+
+        if (eventState == eInputStates.Down)
+            Debug.Log("Undo initiated");
+        if (eventState == eInputStates.Up)
+            Debug.Log("Undo Ended");
+    }
+
+    #endregion
+}
