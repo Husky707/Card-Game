7fa0251daa6bd98bf7c76fefc12de0668cf0ced5
William <gwilliaustin@gmail.com>
2020-10-30 19:36:18 -0500

    Added State Machine

---
+++ Assets/Scripts/StateMachine/StateCore/State.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+public abstract class State : MonoBehaviour
+{
+    public event Action Entered = delegate { };
+    public event Action Exited = delegate { };
+    
+
+    public virtual void Enter() { }
+    public virtual void Exit() { }
+    public virtual void Tick() { }
+}
---
+++ Assets/Scripts/StateMachine/StateCore/StateMachine.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public abstract class StateMachine : MonoBehaviour
+{
+    public State CurrentState => _currentState;
+    public bool InTransition { get; private set; } = false;
+
+    private State _currentState;
+    protected State previousState;
+    private State queuedState;
+
+    private void Update()
+    {
+        if(CurrentState != null && !InTransition)
+        {
+            CurrentState.Tick();
+        }
+    }
+
+    public void ChangeState<T>(GameObject fromObj) where T: State
+    {
+        T targetState = fromObj.GetComponent<T>();
+        if(targetState == null)
+        {
+            Debug.Log("Could not find target state on the " + fromObj.name + " game object");
+            return;
+        }
+
+        BeginStateChange(targetState);
+    }
+    public void ChangeState<T>() where T: State
+    {
+        ChangeState<T>(this.gameObject);
+    }
+
+    public void RevertState()
+    {
+        if (previousState == null)
+            return;
+
+        BeginStateChange(previousState);
+    }
+
+    private void BeginStateChange(State toState)
+    {
+        if(_currentState != toState && !InTransition)
+        {
+            Transition(toState);
+        }
+    }
+
+    private void Transition(State toState)
+    {
+        InTransition = true;
+        queuedState = toState;
+
+        _currentState.Exited += OnStateExited;
+        _currentState.Exit();
+    }
+
+    private void OnStateExited( )
+    {
+        _currentState.Exited -= OnStateExited;
+        previousState = _currentState;
+        _currentState = null;
+
+        EnterNewState(queuedState);
+    }
+
+    private void OnStateEntered()
+    {
+        _currentState = queuedState;
+        queuedState.Entered -= OnStateEntered;
+
+        InTransition = false;
+    }
+
+    private void EnterNewState(State state)
+    {
+        if(state == null)
+        {
+            Debug.Log("State died in queue.");
+            return;
+        }
+
+        queuedState = state;
+        state.Entered += OnStateEntered;
+        state.Enter();
+    }
+  
+}
